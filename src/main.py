850b6676be085f305429879a26aa45474480803f
Mehrad <smortazavi3@ucmerced.edu>
2022-09-16 11:53:14 -0700

    data concat success

--- src/main.py
+++ src/main.py
@@ -1,3 +1,44 @@
-import pandas as p
+# %%
+import pandas as pd
+import os
+import glob
 
-print('this is to confirm new edits are included')
+##### Constants #######
+rtdr = 'C:\\Users\\Students\\Box\\Research' \
+            + '\\IoT4ag\\Project_ Water Stress' \
+                + '\\Data Collection\\Almond\\Ground Data' \
+                    + '\\Almond_sap_weather\\Almond_data'
+                
+sensor_num = 6
+
+##### Class #####
+
+class file_handle:
+    def __init__(self, root, sensor_num):
+        self.dir = root     # Root directory to the raw data
+        self.num = sensor_num        # Number of sap sensors
+
+    def file_mixer_sap(self):
+        files = glob.glob(self.dir + '\\Data_TREWid' + '*.csv')
+        pack = pd.concat(map(pd.read_csv, files), ignore_index=True)
+        # print(pack)
+        return pack
+
+    def file_mixer_weather(self):
+        files = glob.glob(self.dir + '\\Data_weather' + '*.csv')
+        pack = pd.concat(map(pd.read_csv, files), ignore_index=True)
+        # print(pack)
+        return pack    
+
+
+#### Calling Class ####
+
+call = file_handle(rtdr,sensor_num)
+sap_data = call.file_mixer_sap()
+weather_data = call.file_mixer_weather()
+
+# %%
+
+
+
+# %%
